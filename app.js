7d49dcc1914cfc67238a6b93d75da5449f331556
Hostok <51423455+Hostok@users.noreply.github.com>
2019-08-16 10:28:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -60,40 +60,6 @@ function init() {
     mqtt.connect(options); // Conecta ao Broker MQTT
 }
 
-/* Configuracao do Gauge */
-var gauge = c3.generate({
-    bindto: '#gauge',
-    data: {
-        columns: [
-            ['temperature', 0]
-        ],
-        type: 'gauge',
-    },
-    gauge: {
-        label: {
-            format: function (value, ratio) {
-                return value + ' ºC';
-            },
-            show: false
-        },
-        min: 0,
-        max: 50,
-        units: ' ºC',
-    },
-    color: {
-        pattern: ['#227EAF', '#F97600'],
-        threshold: {
-            unit: 'ºC',
-            max: 50,
-            values: [10, 30]
-        }
-    },
-    size: {
-        height: 180
-    }
-});
-
-
 /* App */
 $(document).ready(function () {
     $('#broker').val(json.broker);
