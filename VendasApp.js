e0ceb148d39070ac43c3be254043a093465d4b2f
Hostok <51423455+Hostok@users.noreply.github.com>
2019-08-26 08:27:58 -0300

    Update VendasApp.js

--- VendasApp.js
+++ VendasApp.js
@@ -1,70 +1,111 @@
-/* json com configuracoes iniciais de conexao */
-var json = {
+(function () {
+
+  const brokerInput = document.getElementById('broker');
+  const portInput   = document.getElementById('port');
+  const topicInput  = document.getElementById('topic');
+
+  const saveButton    = document.getElementById('save');
+  const connectButton = document.getElementById('connect');
+
+  /* json com configuracoes iniciais de conexao */
+  let json = {
     broker: 'iot.eclipse.org',
     topic: 'SENAISPTCC/40/SIMV/Vendas',
     port: 1883
-};
+  };
 
-/* Instancia o paho-mqtt */
-var mqtt = new Paho.MQTT.Client(
-    json.broker,
-    parseInt(json.port),
-    "SENAISPTCC-" + Date.now()
-);
+  /* resgata as informações do localStorage caso existir */
+  if ( JSON.parse(localStorage.getItem('mqtt')) ) {
+    json = JSON.parse(localStorage.getItem('mqtt'));
+  }
 
-/* define aos eventos seus respectivos callbacks*/
-mqtt.onConnectionLost = onConnectionLost;
-mqtt.onMessageArrived = onMessageArrived;
+  const mqttConnect = () => {
+    return new Paho.MQTT.Client(
+      json.broker,
+      parseInt(json.port),
+      "SENAISPTCC-" + Date.now()
+    );
+  };
 
-function onConnectionLost(responseObject) {
-    return console.log("Status: " + responseObject.errorMessage);
-}
-function onMessageArrived(Vendas) {
-    var msg = Vendas.payloadString;
+  const onConnectionLost = (responseObject) => {
+    let errorMessage = responseObject.errorMessage;
+    console.log("Status: " + errorMessage);
+    Materialize.toast(errorMessage, 2000);
+
+  };
+
+  const onMessageArrived = (Vendas) => {
+    let msg = Vendas.payloadString;
     console.log(Vendas.destinationName, ' -- ', msg);
 
-/* define aos eventos de Conexão seus respectivos callbacks*/
-var options = {
-    timeout: 3,
-    onSuccess: onSuccess,
-    onFailure: onFailure
-};
-function onSuccess() {
-    console.log("Conectado com o Broker MQTT");
-    mqtt.subscribe(json.topic, {qos: 1}); // Assina o Tópico
-    Materialize.toast('Conectado ao broker', 1000);
-}
+    if ( msg > 50 ) {
+      return false;
+    }
 
-function onFailure(message) {
+    if ( msg == gauge.data.values('temperature')[0] ) {
+      return false;
+    }
+
+    // metodo responsável por atualizar o Gauge
+    gauge.load({
+      columns: [
+        ['temperature', msg]
+      ]
+    });
+
+  };
+
+  /* Instancia o paho-mqtt */
+  let mqtt              = mqttConnect();
+  mqtt.onConnectionLost = onConnectionLost;
+  mqtt.onMessageArrived = onMessageArrived;
+
+
+  const onSuccess = () => {
+    mqtt.subscribe(json.topic, { qos: 1 }); // Assina o Tópico
+    Materialize.toast('Conectado ao broker', 2000);
+  };
+
+  const onFailure = (message) => {
     console.log("Connection failed: " + message.errorMessage);
-}
+  };
 
-/* função de conexão */
-function init() {
+  /* função de conexão */
+  const connect = () => {
+
+    /* define aos eventos de Conexão seus respectivos callbacks*/
+    let options = {
+      timeout: 3,
+      onSuccess: onSuccess,
+      onFailure: onFailure
+    };
+
     mqtt.connect(options); // Conecta ao Broker MQTT
-}
+  };
 
-/* App */
-$(document).ready(function () {
-    $('#broker').val(json.broker);
-    $('#port').val(json.port);
-    $('#topic').val(json.topic);
+  const save = () => {
+    var broker, topic;
+    broker = $('#broker').val();
+    port   = $('#port').val();
+    topic  = $('#topic').val();
 
-    /* Eventos de configuração */
-    $('#save').on('click', function () {
-        var broker, topic;
-        broker = $('#broker').val();
-        port = $('#port').val();
-        topic = $('#topic').val();
+    /* salva no localStorage os dados do formulário */
+    localStorage.setItem("mqtt", JSON.stringify({ broker: broker, port: port, topic: topic }));
 
-        /* salva no localStorage os dados do formulário */
-        localStorage.setItem("mqtt", JSON.stringify({broker: broker, port: port, topic: topic}));
+    return location.reload();
+  };
 
-        location.reload();
-        return false;
-    });
+  const init = () => {
+    brokerInput.value = json.broker;
+    portInput.value   = json.port;
+    topicInput.value  = json.topic;
+  };
 
-    $('#connect').on('click', function () {
-        init();
-    });
-});
+  /* App */
+  init();
+
+  /* Eventos de configuração */
+  saveButton.addEventListener('click', save);
+  connectButton.addEventListener('click', connect);
+
+})();
